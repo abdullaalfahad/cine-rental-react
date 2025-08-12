ab4632e700c0fd6da834e6d1c75d257c3e74aca2
abdullaalfahad <aafahad02@gmail.com>
2025-08-12 23:13:09 +0600

    feat: header added

---
+++ src/components/Header.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import logo from "../assets/logo.svg";
+import ring from "../assets/ring.svg";
+import sun from "../assets/icons/sun.svg";
+import shoppingCart from "../assets/shopping-cart.svg";
+
+export default function Header() {
+  return (
+    <header className="container mx-auto">
+      <nav className="container flex items-center justify-between space-x-10 py-6">
+        <img src={logo} width="139" height="26" alt="" />
+
+        <ul className="flex items-center space-x-5">
+          <li>
+            <a
+              className="bg-primary/20 dark:bg-primary/[7%] rounded-lg backdrop-blur-[2px] p-1 inline-block"
+              href="#"
+            >
+              <img src={ring} width="24" height="24" alt="ring" />
+            </a>
+          </li>
+          <li>
+            <a
+              className="bg-primary/20 dark:bg-primary/[7%] rounded-lg backdrop-blur-[2px] p-1 inline-block"
+              href="#"
+            >
+              <img src={sun} width="24" height="24" alt="sun" />
+            </a>
+          </li>
+          <li>
+            <a
+              className="bg-primary/20 dark:bg-primary/[7%] rounded-lg backdrop-blur-[2px] p-1 inline-block"
+              href="#"
+            >
+              <img
+                src={shoppingCart}
+                width="24"
+                height="24"
+                alt="shopping-cart"
+              />
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
