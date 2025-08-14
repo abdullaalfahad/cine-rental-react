6b2168b2f3c81224e8b134e6ccd4cc76b1a20198
abdullaalfahad <aafahad02@gmail.com>
2025-08-14 06:06:44 +0600

    feat: cart details added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,10 +3,16 @@ import logo from "../assets/logo.svg";
 import ring from "../assets/ring.svg";
 import sun from "../assets/icons/sun.svg";
 import shoppingCart from "../assets/shopping-cart.svg";
+import { useState } from "react";
+import CartDetails from "./CartDetails";
 
 export default function Header() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
   return (
     <header className="container mx-auto">
+      {isModalOpen && <CartDetails onClose={() => setIsModalOpen(false)} />}
+
       <nav className="container flex items-center justify-between space-x-10 py-6">
         <img src={logo} width="139" height="26" alt="" />
 
@@ -31,6 +37,7 @@ export default function Header() {
             <a
               className="bg-primary/20 dark:bg-primary/[7%] rounded-lg backdrop-blur-[2px] p-1 inline-block"
               href="#"
+              onClick={() => setIsModalOpen(true)}
             >
               <img
                 src={shoppingCart}
